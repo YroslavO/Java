6a21fddcd4acb860e6d4c59692e8456728ef0fe8
unknown <ogienko41@gmail.com>
2021-03-28 01:05:46 +0200

    input

---
+++ Java/src/Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("input");
+        String string = s.nextLine();
+        System.out.println("your input "+string );
+    }
+}
