fd8c7ff1cac66a984ef0d143f03a2a85ae7fe8cb
unknown <ogienko41@gmail.com>
2021-03-27 23:28:31 +0200

    while

---
+++ Java/src/While.java
@@ -0,0 +1,13 @@
+public class While {
+    public static void main (String[] args){
+        int value = 0;
+
+        while (value<5){
+            System.out.println(value);
+            value=value+1;
+
+        }
+
+
+    }
+}
