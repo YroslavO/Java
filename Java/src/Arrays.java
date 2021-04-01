33bd7c57bf700d5e018c1aabdf688d3d5b6fd958
unknown <ogienko41@gmail.com>
2021-04-02 00:32:08 +0300

    doWhile

--- Java/src/Arrays.java
+++ Java/src/Arrays.java
@@ -1,5 +1,15 @@
 public class Arrays {
     public static void main(String[] args){
-        
+        int number = 10;
+        int [] numbers = new int [5];
+        for (int i = 0;i<numbers.length;i++){
+            numbers[i] = i*10;
+            System.out.println(numbers[i]);
+        }
+             int[] numbers2 = {1,2,3,4};
+        for(int i=0;i<numbers2.length;i++){
+            System.out.println(numbers2[i]);
+        }
+
     }
 }
--- Java/src/DuWhile.java
+++ Java/src/DuWhile.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class DuWhile {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        int value ;
+        int i ;
+        i = scanner.nextInt();
         do{
-            System.out.println("input 5");
-             value = scanner.nextInt();
 
-        }while (value!=5);
-        System.out.println("correct");
+
+            if (i%3==0){
+                continue;
+            }
+            System.out.println(i);
+            i--;
+        }while (i>=0);
+
     }
 }
