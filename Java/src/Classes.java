5870f6f960feb0539e49000747cff882e8fd33d3
unknown <ogienko41@gmail.com>
2021-04-04 00:32:40 +0300

    classes 2

--- Java/src/Classes.java
+++ Java/src/Classes.java
@@ -3,18 +3,29 @@ public class Classes {
         Person person1 = new Person();
         person1.name = "Roma";
         person1.age = 50;
-
-        System.out.println("My name "+person1.name+","+"my age "+person1.age);
+        person1.speak();
+        person1.seyHello();
 
         Person person2 = new Person();
         person2.name = "Max";
         person2.age = 28;
+        person2.speak();
 
-        System.out.println("My name "+person2.name+","+"my age "+person2.age);
     }
 }
 
 class Person{
     String name;
     int age;
+
+    void speak(){
+        for(int i = 0;i<3;i++){
+            System.out.println("my name "+name+",my"+age);
+        }
+
+        }
+    void seyHello(){
+        System.out.println("Hello");
+    }
+
 }
