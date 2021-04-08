a91b2c749fcc0887ad67fada748123e8f4ad1e77
unknown <ogienko41@gmail.com>
2021-04-09 01:10:09 +0300

    get-set

--- Java/src/Classes.java
+++ Java/src/Classes.java
@@ -1,40 +1,78 @@
 public class Classes {
     public static void main( String[] args ) {
         Person person1 = new Person();
-        person1.name = "Roma";
-        person1.age = 50;
-        person1.speak();
-        person1.seyHello();
 
-        Person person2 = new Person();
-        person2.name = "Max";
-        person2.age = 28;
-        person2.speak();
-        int year1 = person1.calculate();
-        System.out.println("out "+year1);
-        int year2 = person2.calculate();
-        System.out.println("out "+year2);
+        person1.setName("");
+        person1.setAge(-1);
+        System.out.println("out information name "+person1.getName());
+        System.out.println("out information age "+person1.getAge());
+        person1.getAge();
+
+
+
+
     }
 }
 
 class Person{
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName){
+        if (userName.isEmpty()){
+            System.out.println("need name");}
+        else{
+            name = userName;
+        }
+
+    }
+
+    public String getName(){
+        return  name;
+    }
+
+    public void setAge(int userAge){
+        if(userAge<=0){
+            System.out.println("need age");
+        }else{
+            age = userAge;
+        }
+
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+
     int calculate(){
         int years = 65-age;
         return years;
 
-
-
     }
+    void setNameAndAge(String username, int userage){
+        name = username;
+        age =  userage;
+    }
+
+
+
+
     void speak(){
         for(int i = 0;i<3;i++){
-            System.out.println("my name "+name+",my"+age);
+            System.out.println("my name "+name+",my age "+age+" years");
         }
 
         }
+
+
+
     void seyHello(){
         System.out.println("Hello");
     }
+
+
+
 
 }
