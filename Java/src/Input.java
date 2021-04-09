fa56636c646268fed36f07ea1bdefcb5a8e5b234
unknown <ogienko41@gmail.com>
2021-04-10 00:24:56 +0300

    v0.1

--- Java/src/Input.java
+++ Java/src/Input.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Input {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        System.out.println("input");
+
         String string = s.nextLine();
         System.out.println("your input "+string );
     }
---
+++ Java/src/MyProject.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MyProject {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        String input = in.nextLine();
+        System.out.println(input);
+
+
+
+    }
+
+}
