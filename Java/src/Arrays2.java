5af1fb1685a8c7a6842dd5a6e42fc0186c01519c
unknown <ogienko41@gmail.com>
2021-05-12 01:20:19 +0300

    switch2

---
+++ Java/src/Arrays2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Arrays2 {
+    public static void main (String[] args) {
+        int[] numbers = new int[10];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10;
+            System.out.println(numbers[i]);
+        }
+        int[] x = {1, 23, 2343, 353, 24234, 242};
+        for (int n = 0 ;n < x.length ;n++){
+            System.out.println(x[n]);
+        }
+
+
+    }
+
+}
--- Java/src/Experement.java
+++ Java/src/Experement.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
-
 public class Experement {
-    public static void main (String [] args){
-        Scanner x = new Scanner(System.in);
-
-        while(true){
-            int i = x.nextInt();
+public static void main(String[] args){
+    Scanner x = new Scanner(System.in);
+    int n = x.nextInt();
+    int i = 0;
+    while(i<=n){
+        if(i%2==0){
             System.out.println(i);
+            i++;
+            continue;
         }
-
+        i++;
     }
+}
 }
