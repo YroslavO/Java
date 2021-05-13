3b477ab462ef05f2f6fef32d8a6136e32cccdfc8
unknown <ogienko41@gmail.com>
2021-05-14 00:27:37 +0300

    foritch

--- Java/src/AandS.java
+++ Java/src/AandS.java
@@ -1,24 +1,16 @@
 public class AandS {
     public static void main (String[] args){
-        int [] numbers = new int [5];
-        numbers[0] = 10;
-        String [] strings = new String[3];
-        strings[0] = "Hello";
-        strings[1] = "Buy";
-        strings[2] = "Java";
-        int sum = 0;
-        for(int i = 0;i<strings.length;i++){
-            System.out.println(strings[i]);
+        String[] x = new String[10];
+        String y = "i";
+        String sum = new String();
+        for (int i =0;i<x.length ;i++){
+
+            x[i] = "i";
+            sum = x[i]+sum;
+            System.out.println(sum);
         }
-        for(String string:strings){
-            System.out.println(string);
-        int [] x = {1,2,3,4,5,6};
-        
-
-        for (int x1:x){
-            sum = sum+x1;
-            }
-
-        }System.out.println(sum);
+        for(String i:x){
+            System.out.println(sum);
+        }
     }
 }
