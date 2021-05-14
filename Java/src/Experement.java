d40dd315a1d655d99d8137ccd40ff5ceb5b2a67a
unknown <ogienko41@gmail.com>
2021-05-15 01:18:17 +0300

    number_length

--- Java/src/Experement.java
+++ Java/src/Experement.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
+
 public class Experement {
 public static void main(String[] args){
+Scanner put = new Scanner(System.in);
+    System.out.println("put number");
+int number = put.nextInt();             // ввод целого числа
+int sum = 0;                            // создание переменной-счетчик
 
-         Scanner scanner = new Scanner(System.in);
-         int n;
-    System.out.println("Please input size arrays");
-         n = scanner.nextInt();
-         int[] arrays = new int [n];
-         arrays[9] = 10;
+String str = Integer.toString(number);  // конвертация целочисленной переменной в строковую
+int j = str.length();    // получение длины строковой переменной для установления границ цикла 
 
-          for(int i=0;i<arrays.length;i++ ){
+for (int i =0;i<j;i++){
+    int x = number % 10; // вычисляем последний элемент числа
+    sum = sum+x;         // добавляем последний элемент к переменной-счетчик
+    number = number/10;  // отнимаем последний элемент чила
 
-              arrays[i] = i*10;
-              System.out.println(arrays[i]);
-          }
+}
+    System.out.println(sum);        // Вывод ссумы всех цифр в числе
 
 
 
