e0ddc9d3c04c02391ae8e1145bb6a701196969da
unknown <ogienko41@gmail.com>
2021-05-19 23:54:27 +0300

    Classes

---
+++ Java/src/ClassesAndObjects.java
@@ -0,0 +1,14 @@
+public class ClassesAndObjects {
+    public static void main (String[] args){
+        Person you = new Person();
+        you.name = "Yroslav";
+    }
+}
+class Person{
+    String name;
+    int age;
+
+    // у класса могут быть
+    //1.данные (поля)
+    //2.действия которые он может совершать(методы)
+}
