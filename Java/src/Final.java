00161dfd24c1df20490448998d29418c0124b09c
unknown <ogienko41@gmail.com>
2021-05-28 00:16:42 +0300

    toString

--- Java/src/Final.java
+++ Java/src/Final.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
 public class Final {
     public static void main( String[] args ) {
-        final int X = 10;
-        System.out.println(X);
+        Scanner put = new Scanner(System.in);
+        System.out.println("Put your name");
+        String x = put.nextLine();
+        System.out.println("Put your age");
+        int y = put.nextInt();
+        System.out.println();
+        if(x.length()==0){
+            Test2 you = new Test2();
+        }else {
+            Test2 you = new Test2(x,y);
+        }
+
     }
 }
-class Test{
-    public static final int CONSTANT = 0;
+class Test2{
+   private String name;
+   private int age;
 
 
-}
+    public Test2(){
+        System.out.println("You do not enter name of age");
+    }
+
+
+   public Test2(String name,int age){
+       this.name = name;
+       this.age = age;
+       System.out.println("Hello "+name+" ! And your age "+age+" !" );
+   }
+
+   public void setName(String name){
+       this.name = name;
+   }
+   public void setAge(int age){
+       this.age = age;
+   }
+
+   }
---
+++ Java/src/StringBilder.java
@@ -0,0 +1,11 @@
+public class StringBilder {
+    public static void main( String[] args ) {
+        StringBuilder sb =new StringBuilder("Hello");
+        System.out.println(sb.toString());
+        sb.append(" my").append(" friend");
+        System.out.println(sb.toString());
+        int y = 15000;
+        String x;
+        System.out.printf("%d",y);
+    }
+}
