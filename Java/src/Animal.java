7e6eebdc573ef2fa7109af7b2afe29ecf731306d
unknown <ogienko41@gmail.com>
2021-05-29 00:55:00 +0300

    interface

---
+++ Java/src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+
+    String name;
+
+    public void sleep(){
+        System.out.println("I am sleeping");
+    }
+    public void eat(){
+        System.out.println("I am eating");
+    }
+}
---
+++ Java/src/Interfaces/Person.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public class Person implements Info {
+    public String name;
+    public Person(String name){
+        this.name = name;
+    }
+    public void seyHello(){
+        System.out.println("Hello");
+    }
+    public void showInfo(){
+        System.out.println("Name is "+this.name);
+    }
+}
